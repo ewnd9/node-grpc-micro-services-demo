6db9751a3aa6c50a91f4d8aaf9ccebbdc6c969a6
ewnd9 <ewndnine@gmail.com>
2022-05-04 17:13:01 +0300

    chore: use nice-grpc-server-health

--- packages/shared/grpc-server/src/index.ts
+++ packages/shared/grpc-server/src/index.ts
@@ -1,5 +1,7 @@
 import { createServer as createGrpcServer } from 'nice-grpc';
 import { ServerReflectionService, ServerReflection } from 'nice-grpc-server-reflection';
+import { HealthDefinition, HealthServiceImpl } from 'nice-grpc-server-health';
+
 import fs from 'fs';
 
 export function createServer({
@@ -12,6 +14,7 @@ export function createServer({
   binPath: string;
 }) {
   const server = createGrpcServer();
+  server.add(HealthDefinition, HealthServiceImpl());
   server.add(definition, implementation);
   server.add(ServerReflectionService, ServerReflection(fs.readFileSync(binPath), [definition.fullName]));
 
